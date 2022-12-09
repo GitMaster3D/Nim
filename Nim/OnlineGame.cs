66c96cfcffc2dd839b41a4f97fc795189c4c1089
Richtiger Lappen <germanhandschuhe@gmail.com>
2022-12-09 22:50:49 +0100

    123

--- Nim/OnlineGame.cs
+++ Nim/OnlineGame.cs
@@ -211,10 +211,9 @@ namespace Nim
         }
 
 
-
         public enum Players
         {
-            Player1= 0,
+            Player1 = 0,
             Player2 = 1,
             Player3 = 2,
             Player4 = 3
